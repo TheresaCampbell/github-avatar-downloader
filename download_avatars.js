a740bde449b2d661f62d78d308962edfe43348c9
Theresa Campbell <t.campbell@sasktel.net>
2018-06-11 22:08:54 +0000

    Authorization

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,19 @@
 var request = require("request");
+var authorization = require("./secrets.js");
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      "User-Agent": "request"
+    }
+  };
+
+request(options, function(err, res, body) {
     cb(err, body);
-  })
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
