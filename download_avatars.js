8d44dcbd80e589ad0928222895592a483d82f4b6
Theresa Campbell <t.campbell@sasktel.net>
2018-06-11 23:03:03 +0000

    Printing all avatar urls.

--- download_avatars.js
+++ download_avatars.js
@@ -11,12 +11,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-request(options, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, result, body) {
+    var contributors = JSON.parse(body);
+    cb(err, contributors);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors: ", err);
-  console.log("Result: ", result);
+  for (var i = 0; i < result.length; i++) {
+    console.log(result[i].avatar_url);
+  }
 });
