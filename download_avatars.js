0606bd48449833bed29ccee90bf0f04418b00e80
Theresa Campbell <t.campbell@sasktel.net>
2018-06-12 00:12:54 +0000

    Takes in command line arguments.

--- download_avatars.js
+++ download_avatars.js
@@ -1,33 +1,39 @@
 var request = require("request");
 var authorization = require("./secrets.js");
 var fs = require("fs");
+args = process.argv.slice(2);
 
-// console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var options = {
-//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//     headers: {
-//       "User-Agent": "request"
-//     }
-//   };
 
-//   request(options, function(err, result, body) {
-//     var contributors = JSON.parse(body);
-//     cb(err, contributors);
-//   });
-// }
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      "User-Agent": "request"
+    }
+  };
 
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors: ", err);
-//   for (var i = 0; i < result.length; i++) {
-//     console.log(result[i].avatar_url);
-//   }
-// });
+
+  request(options, function(err, result, body) {
+    var contributors = JSON.parse(body);
+    cb(err, contributors);
+  });
+}
+
+
+getRepoContributors(args[0], args[1], function(err, result) {
+  console.log("Errors: ", err);
+  for (var i = 0; i < result.length; i++) {
+    var avatarURL = result[i].avatar_url;
+    var path = result[i].login + ".png";
+    downloadImageByURL(avatarURL, "./avatars/" + path);
+  }
+});
+
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
          .pipe(fs.createWriteStream(filePath))
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg");
